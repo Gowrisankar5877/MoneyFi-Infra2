692198895ed14061efde9aaab24c09c51cfd8278
devuser12 <devuser12@refabs365.com>
2025-06-21 06:56:19 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,18 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.105.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
+
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
