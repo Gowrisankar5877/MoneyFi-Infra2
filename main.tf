925971328c3713e4b242819689b28fd465b2d08a
devuser12 <devuser12@refabs365.com>
2025-06-21 11:34:20 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -442,7 +442,7 @@ resource "azurerm_key_vault" "kv" {
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
   purge_protection_enabled    = true
-  soft_delete_enabled         = true
+  #soft_delete_enabled         = true
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
