8f7da1919acf041a551aafe33d700c51297526ce
devuser12 <devuser12@refabs365.com>
2025-06-21 09:20:36 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ terraform {
 # -------------------------
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
-  location = var.location
+  location = "australiaeast"
 }
 
 # -------------------------
