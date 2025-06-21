089c38118211cc1180fc266ff5c972c59dd63b03
devuser12 <devuser12@refabs365.com>
2025-06-21 07:40:32 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -9,21 +9,20 @@ terraform {
       version = "~> 3.105.0"
     }
   }
-
   required_version = ">= 1.3.0"
 }
 
-
+# -------------------------
+# Resource Group
+# -------------------------
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
 }
 
-
-
-#######################
-# Central India VNet
-#######################
+# -------------------------
+# VNet 1 - Central India
+# -------------------------
 resource "azurerm_virtual_network" "vnet_central" {
   name                = "MoneyFi-Vnet1"
   address_space       = ["10.10.0.0/16"]
@@ -38,12 +37,6 @@ resource "azurerm_subnet" "subnet1_central" {
   address_prefixes     = ["10.10.1.0/24"]
 }
 
-resource "azurerm_subnet_network_security_group_association" "assoc_subnet1_central" {
-  subnet_id                 = azurerm_subnet.subnet1_central.id
-  network_security_group_id = azurerm_network_security_group.nsg_private.id
-}
-
-
 resource "azurerm_subnet" "subnet2_central" {
   name                 = "MoneyFi-V1S2"
   resource_group_name  = azurerm_resource_group.example.name
@@ -51,14 +44,9 @@ resource "azurerm_subnet" "subnet2_central" {
   address_prefixes     = ["10.10.2.0/24"]
 }
 
-resource "azurerm_subnet_network_security_group_association" "assoc_subnet2_central" {
-  subnet_id                 = azurerm_subnet.subnet2_central.id
-  network_security_group_id = azurerm_network_security_group.nsg_private.id
-}
-
-#######################
-# West India VNet
-#######################
+# -------------------------
+# VNet 2 - West India
+# -------------------------
 resource "azurerm_virtual_network" "vnet_west" {
   name                = "MoneyFi-Vnet2"
   address_space       = ["10.20.0.0/16"]
@@ -72,11 +60,6 @@ resource "azurerm_subnet" "subnet1_west" {
   virtual_network_name = azurerm_virtual_network.vnet_west.name
   address_prefixes     = ["10.20.1.0/24"]
 }
-resource "azurerm_subnet_network_security_group_association" "assoc_subnet1_west" {
-  subnet_id                 = azurerm_subnet.subnet1_west.id
-  network_security_group_id = azurerm_network_security_group.nsg_private.id
-}
-
 
 resource "azurerm_subnet" "subnet2_west" {
   name                 = "MoneyFi-V2S2"
@@ -85,16 +68,11 @@ resource "azurerm_subnet" "subnet2_west" {
   address_prefixes     = ["10.20.2.0/24"]
 }
 
-resource "azurerm_subnet_network_security_group_association" "assoc_subnet2_west" {
-  subnet_id                 = azurerm_subnet.subnet2_west.id
-  network_security_group_id = azurerm_network_security_group.nsg_private.id
-}
-
-##########################
-# NSG - Deny Public Access
-##########################
-resource "azurerm_network_security_group" "nsg_private" {
-  name                = "nsg-private-subnets"
+# -------------------------
+# NSG - Central India
+# -------------------------
+resource "azurerm_network_security_group" "nsg_private_central" {
+  name                = "Moneyfinsg-private-central"
   location            = var.location
   resource_group_name = azurerm_resource_group.example.name
 
@@ -111,7 +89,7 @@ resource "azurerm_network_security_group" "nsg_private" {
   }
 
   security_rule {
-    name                       = "AllowVNetInBound"
+    name                       = "AllowVNetInbound"
     priority                   = 200
     direction                  = "Inbound"
     access                     = "Allow"
@@ -124,7 +102,7 @@ resource "azurerm_network_security_group" "nsg_private" {
 
   security_rule {
     name                       = "AllowAllOutbound"
-    priority                   = 100
+    priority                   = 300
     direction                  = "Outbound"
     access                     = "Allow"
     protocol                   = "*"
@@ -135,3 +113,73 @@ resource "azurerm_network_security_group" "nsg_private" {
   }
 }
 
+# -------------------------
+# NSG - japan west
+# -------------------------
+resource "azurerm_network_security_group" "nsg_private_west" {
+  name                = "Moneyfinsg-private-japanwest"
+  location            = var.secondary_location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "DenyInternetInbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowVNetInbound"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "VirtualNetwork"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+  }
+
+  security_rule {
+    name                       = "AllowAllOutbound"
+    priority                   = 300
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+  }
+}
+
+# -------------------------
+# NSG Associations - Central India
+# -------------------------
+resource "azurerm_subnet_network_security_group_association" "assoc_subnet1_central" {
+  subnet_id                 = azurerm_subnet.subnet1_central.id
+  network_security_group_id = azurerm_network_security_group.nsg_private_central.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "assoc_subnet2_central" {
+  subnet_id                 = azurerm_subnet.subnet2_central.id
+  network_security_group_id = azurerm_network_security_group.nsg_private_central.id
+}
+
+# -------------------------
+# NSG Associations - West India
+# -------------------------
+resource "azurerm_subnet_network_security_group_association" "assoc_subnet1_west" {
+  subnet_id                 = azurerm_subnet.subnet1_west.id
+  network_security_group_id = azurerm_network_security_group.nsg_private_west.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "assoc_subnet2_west" {
+  subnet_id                 = azurerm_subnet.subnet2_west.id
+  network_security_group_id = azurerm_network_security_group.nsg_private_west.id
+}
