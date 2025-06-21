d3e79564ba357e23a0b3d9d8ab556fce1bec0ebb
devuser12 <devuser12@refabs365.com>
2025-06-21 10:50:03 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -318,7 +318,7 @@ resource "azurerm_mssql_database" "db" {
   server_id      = azurerm_mssql_server.sql.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   sku_name       = "Basic"
-  max_size_gb    = 10
+  max_size_gb    = 2
 }
 
 # -------------------------
